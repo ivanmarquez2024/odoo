197549c380349680c5873d87c3a1012837f89d7a
Cesar Campos <74711471+ccamposidi@users.noreply.github.com>
2021-12-28 09:02:57 -0600

    Modulo Carta porte

---
+++ l10n_mx_traslado/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import autotransporte
+from . import factura_traslado
+from . import product
+from . import res_partner
+from . import res_company
+from . import stock_picking
