db66f6f21e37fbead3f178e15d5f1bd467861ff7
ivanmarquez2024 <157522254+ivanmarquez2024@users.noreply.github.com>
2024-02-09 12:28:25 -0600

    Delete l10n_mx_traslado directory

--- l10n_mx_traslado/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import autotransporte
-from . import factura_traslado
-from . import product
-from . import res_partner
-from . import res_company
-from . import stock_picking
