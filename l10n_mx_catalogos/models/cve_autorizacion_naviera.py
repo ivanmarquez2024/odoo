e1e9de135a7cbf870c6ef094cf3a748a95c40260
ivanmarquez2024 <157522254+ivanmarquez2024@users.noreply.github.com>
2024-02-09 12:28:57 -0600

    Delete l10n_mx_catalogos directory

--- l10n_mx_catalogos/models/cve_autorizacion_naviera.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class CveAurotizacionNaviera(models.Model):
-    _name = 'cve.autorizacion.naviera'
-    _rec_name = "autorizacion_naviera"
-
-    autorizacion_naviera = fields.Char(string='Número de autorización')
