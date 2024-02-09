0e134de74b9040d6244125b35d83671c9c9a5207
ivanmarquez2024 <157522254+ivanmarquez2024@users.noreply.github.com>
2024-02-09 12:29:47 -0600

    Delete complemento_exterior directory

--- complemento_exterior/models/product.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models, api,_
-    
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    fraccionarancelaria = fields.Many2one('catalogos.fraccionarancelaria', string='Fracción Arancelaria')
-    unidadAduana = fields.Many2one('catalogos.unidadmedidaaduana', string='Unidad aduana')
