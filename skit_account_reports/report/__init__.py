bba438efc88b633f9b8b8a04c46f0df8d031f426
ivanmarquez2024 <157522254+ivanmarquez2024@users.noreply.github.com>
2024-02-09 12:27:22 -0600

    Delete skit_account_reports directory

--- skit_account_reports/report/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import account_report_financial
-from . import account_balance
-from . import account_general_ledger
-from . import account_aged_partner_balance
-from . import account_partner_ledger
-from . import account_journal
-from . import account_tax
